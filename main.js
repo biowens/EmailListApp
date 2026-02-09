8f91473c912fb9b70b5307558b26c137dc3608df
kit <athoof@gmail.com>
2026-02-09 23:33:15 +0500

    Remove unused test function from main.js
    Remove the test function and its related DOM manipulations.

--- main.js
+++ main.js
@@ -30,13 +30,3 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
-function test() {
-  var opt = document.createElement("option");
-  opt.value = "testOption";
-  opt.innerHTML = "Test Option";
-
-  document.getElementById("testSelect").append(opt);
-  document.getElementById("testSelect").removeAttribute("disabled");
-  document.getElementById("testLabel").innerHTML = "TESTTESTTEST";
-}
